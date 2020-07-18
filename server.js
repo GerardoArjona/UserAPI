a418fd6b3a0002f0ce4c6b189c100753a33daaca
Gerardo Arjona <gerardo.arj15@gmail.com>
2020-07-17 22:43:00 -0500

    npm install, add server js
    Signed-off-by: Gerardo Arjona <gerardo.arj15@gmail.com>

---
+++ server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+const express = require('express')
+const bodyParser = require('body-parser');
+const morgan = require('morgan')
+const mongoose = require('mongoose');
+//const routes =  require('./routes');
+
+const cors = require('cors');
+
+// const MONGO = process.env.MONGODB_URI
+
+// mongoose.connect(MONGO,{ useNewUrlParser: true, useCreateIndex:true });
+// const mongo =  mongoose.connection;
+
+// mongo.on('error', (error) => console.log(error))
+// 	 .once('open', () => console.log("Connected to database"));
+
+
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(cors())
+app.use(morgan('dev'));
+
+//app.use('/api/v1',routes)
+
+app.get('/',(req,res) => {
+	res.send("Hola mundo")
+})
+
+app.listen(PORT,() =>{
+	console.log(`Works in port ${PORT}`)
+});
